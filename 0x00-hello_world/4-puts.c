15fb646e7a993c95fa2e10109893a4a8beb7cd9a
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 00:24:03 +0100

    Fixed an error to be betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-int main()
-{
-	puts("Programming is like building a multilingual puzzle\n"); /*Printing a string*/
-	return 0;
-}
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle\n"); /*Printing a string*/
+  return (0); }

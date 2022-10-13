e2f7cab379e356ef8072d9db483eacb761cbdae2
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 00:32:41 +0100

    **********

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*Hello, I am learning to use puts() */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /*Printing a string*/
+	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0);
 }

48d906889a4cc5add2454c1112f22a4d566e6cf0
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 00:56:19 +0100

    Use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *Return: returns 0 on success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

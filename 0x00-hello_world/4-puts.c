0bca659c04ea09e0168ee1440c89b4982700271f
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 00:27:36 +0100

    Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
 #include <stdio.h>
-int main(void)
-{ puts("\"Programming is like building a multilingual puzzle\n"); /*Printing a string*/
-  return (0); }
+int main(void){puts("\"Programming is like building a multilingual puzzle\n"); /*Printing a string*/
+return (0);}

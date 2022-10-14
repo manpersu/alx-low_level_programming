583a9244b4ec6db44542b172ec0c43769632bcd2
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 02:02:31 +0100

    Output an error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point into the function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}

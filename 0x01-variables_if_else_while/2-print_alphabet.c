7c6cbb2954a3f5387557678d63072f8092f816f9
Manpasu Mathias <manpersu@protonmail.com>
2022-10-14 23:38:10 +0100

    Pint all alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: return 0 on success
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}

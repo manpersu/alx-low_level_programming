733e44dc85a2bb25440886b4adac5135da1f929d
Manpasu Mathias <manpersu@protonmail.com>
2022-11-05 16:44:47 +0100

    just libraties

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ *  main - Entry Point.
+ *  Description: The main function prints strings using the _puts() func.
+ *
+ *  _puts - This function prints strings in the main
+ *
+ *  Return - return 0 on success
+ */
+nt main(void)
+{
+	_puts("\"At the end of the day, my goal was to be \
+			the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}

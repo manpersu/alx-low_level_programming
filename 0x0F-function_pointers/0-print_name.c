9196269d52dc17b25a4d4766d7c7b8d554c38c74
Manpasu Mathias <manpersu@protonmail.com>
2022-11-16 22:34:19 +0100

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdilib.h>
+#include <functional_pointers.h>
+/**
+ * print_name - prints the name passed as an argument
+ * @name: the name passed to the function as a parameter
+ * @f: function pointer that serve as a callback
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+	}
+	else
+	{
+		f(name);
+	}
+}

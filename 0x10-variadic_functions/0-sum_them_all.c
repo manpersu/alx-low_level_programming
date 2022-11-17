825c51ad0b8553d8adc48a5c2cc4fc1f26ba211e
Manpasu Mathias <manpersu@protonmail.com>
2022-11-17 20:14:48 +0100

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+/**
+ * sum_them_all - It sum all the variable arguments
+ * @n:  Arguments to sum
+ *
+ * Return: Returns the sum of the arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	va_list va;
+	int sum, i;
+
+	va_start(va, n);/* Initialize the argument list. */
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(va, int);/* Get the next argument value. */
+	va_end(va);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,12 @@
+#0x10-variadic_functions
+
+**General**
+. What are variadic functions
+. How to use va_start, va_arg and va_end macros
+. Why and how to use the const type qualifier
+
+##Exercises:
+**0. Beauty is variable, ugliness is constant
+	*Write a function that returns the sum of all its parameters.
+		. Prototype: int sum_them_all(const unsigned int n, ...);
+		. If n == 0, return 0
